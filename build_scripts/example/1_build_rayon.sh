4016bac875d81a5372bd80dab6c93988d53df750
Joel Berendzen <joel@generisbio.com>
2021-08-26 17:29:12 -0600

    build stage 1

--- build_scripts/example/1_build_rayon.sh
+++ build_scripts/example/1_build_rayon.sh
@@ -9,10 +9,9 @@ if [ -z "$RAYON_BUILD_DIR" ]; then
 fi
 repo_url="https://raw.githubusercontent.com/unmtransinfo/rayon/master"
 # get environmental script
-mkdir -p $RYN_SCRIPT_DIR
-curl -L -o rayon_tool ${repo_url}/rayon/bin/server_env.sh
-mv server_env.sh rayon_env
-chmod 755 rayon_env
+mkdir -p ${RYN_SCRIPT_DIR}/bin
+curl -L -o ${RYN_SCRIPT_DIR}/bin/rayon_env ${repo_url}/rayon/bin/server_env.sh
+chmod 755 ${RYN_SCRIPT_DIR}/bin/rayon_env
 # get build tool
 mkdir -p $RAYON_BUILD_DIR
 cd $RAYON_BUILD_DIR
